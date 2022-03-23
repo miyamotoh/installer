38a3a5405e5cf2a8cf90c63aad10f31543077602
Hiro Miyamoto <miyamotoh@us.ibm.com>
2022-03-23 11:15:41 -0400

    Moving bootstrap to own stage for easier destruction
    Signed-off-by: Hiro Miyamoto <miyamotoh@us.ibm.com>

--- data/data/powervs/bootstrap/lb/main.tf
+++ data/data/powervs/bootstrap/lb/main.tf
@@ -1,26 +1,5 @@
 # Using explicit depends_on as otherwise there are issues with updating and adding of pool members
 # Ref: https://registry.terraform.io/providers/IBM-Cloud/ibm/latest/docs/resources/is_lb_listener
-resource "ibm_is_lb_pool_member" "machine_config_member" {
-  lb             = var.lb_int_id
-  pool           = var.machine_cfg_pool_id
-  port           = 22623
-  target_address = var.bootstrap_ip
-}
-
-resource "ibm_is_lb_pool_member" "api_member_int" {
-  depends_on     = [ibm_is_lb_pool_member.machine_config_member]
-  lb             = var.lb_int_id
-  pool           = var.api_pool_int_id
-  port           = 6443
-  target_address = var.bootstrap_ip
-}
-
-resource "ibm_is_lb_pool_member" "api_member" {
-  lb             = var.lb_ext_id
-  pool           = var.api_pool_ext_id
-  port           = 6443
-  target_address = var.bootstrap_ip
-}
 
 # bootstrap ssh pool, listener, member
 resource "ibm_is_lb_pool" "bootstrap_pool" {
